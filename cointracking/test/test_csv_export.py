d80625b2f94400b94171515d9a84587892967286
dzid26 <dzidmail@gmail.com>
2021-11-07 23:04:27 +0000

    Remove test file after csv export test

--- cointracking/test/test_csv_export.py
+++ cointracking/test/test_csv_export.py
@@ -1,6 +1,6 @@
 #pytest
 
-import time
+import time, os
 from cointracking import CoinTracking_CSV
 
 CSV_REF = """\
@@ -12,12 +12,21 @@ CSV_REF = """\
 """
 
 class TestCsvExport():
+    test_file = "test_export.csv"
     def test_csv_export(self):
-        test_file = "test_export.csv"
-        ct_csv = CoinTracking_CSV(test_file)
+        ct_csv = CoinTracking_CSV(self.test_file)
         ct_csv.add_deposit_to_csv(time.localtime(0), 4, "USD")
         ct_csv.add_withdrawal_to_csv(time.localtime(0), 4, "USD")
         ct_csv.add_staking_to_csv(time.localtime(0), 4, "USD")
         ct_csv.add_otherfee_to_csv(time.localtime(0), 4, "USD")
+
+        assert  [row.rstrip('\n') for row in open(self.test_file)] == [row for row in CSV_REF.split("\n")]
+
+    def teardown_method(self):
+        print('Remove test file')
+        try:
+            os.remove(self.test_file)
+        except:
+            pass
+
         
-        assert  [row.rstrip('\n') for row in open(test_file)] == [row for row in CSV_REF.split("\n")]
